4222c94530849eb8b96acc24a08b9b0e314ca8a1
Gabriel Estruc <84810324+GabrielEstruc@users.noreply.github.com>
2021-05-25 23:36:22 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+	$('.btn1').click(function() {
+		$('.texto1').toggleClass('responsivo');
+	});
+
+	$('.btn2').click(function() {
+		$('.texto2').toggleClass('responsivo');
+
+	});
+
+	$('.btn3').click(function() {
+		$('.texto3').toggleClass('responsivo');
+
+	});
+
+	$('.btn4').click(function() {
+		$('.texto4').toggleClass('responsivo');
+
+	});
+
+	$('.btn5').click(function() {
+		$('.texto5').toggleClass('responsivo');
+
+	});
+
+	$('.btn7').click(function() {
+		$('.texto6').toggleClass('responsivo');
+
+	});
+
+	$('.btn6').click(function() {
+		$('.menu').toggleClass('responsivo');
+	});
+
+		$('.menu').click(function() {
+		$('.menu').toggleClass('responsivo');
+	});
+
+});
+
+var nav = $('.solto');
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            nav.addClass("fixo");
+        } else {
+            nav.removeClass("fixo");
+        }
+    });
+
+
+
+
+
