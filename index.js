36f332d4007965f066985386edcd1197db52101d
Gabriel Estruc <84810324+GabrielEstruc@users.noreply.github.com>
2024-02-16 22:51:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+function traduzirEN(){
+  for(var i = 0; i<lang.en.length; i++){
+    document.getElementById("#" + i).innerHTML = lang.en[i];
+  }
+  document.getElementById("journey").src = "imagens/jornada_en.svg";
+}
+function traduzirPT(){
+ for(var i = 0; i<lang.pt.length; i++){
+    document.getElementById("#" + i).innerHTML = lang.pt[i];
+  }
+    document.getElementById("journey").src = "imagens/jornada.svg";
+}
+
 var data = new Date();
 var nasc = 1994;
 var ano = data.getFullYear();
@@ -12,16 +25,22 @@ if (mes <= 5 && dia < 10){
 document.getElementById("idade").innerHTML = " " + idade + ".";
 }
 
+  function abrirPrototipoMonkey(){
+    if (protMonkey.childElementCount <= 1){
+      document.getElementById("tela").style = "display: none";
+      var prot = document.createElement("iframe");
+      prot.setAttribute("allowfullscreen", "true");
+      prot.setAttribute("src","https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FLYdH3999zmcgteoD1rxwQr%2FMonkeyPlay%3Fpage-id%3D0%253A1%26type%3Ddesign%26node-id%3D1-2%26viewport%3D67%252C434%252C0.23%26t%3DLNfQ7YTvTUsNjGYP-1%26scaling%3Dscale-down%26starting-point-node-id%3D1%253A2%26mode%3Ddesign")
+      document.getElementById("protMonkey").appendChild(prot);
+    } 
+    }
 
-function traduzirEN(){
-  for(var i = 0; i<lang.en.length; i++){
-    document.getElementById("#" + i).innerHTML = lang.en[i];
-  }
-  document.getElementById("journey").src = "imagens/jornada_en.svg";
-}
-function traduzirPT(){
- for(var i = 0; i<lang.pt.length; i++){
-    document.getElementById("#" + i).innerHTML = lang.pt[i];
-  }
-    document.getElementById("journey").src = "imagens/jornada.svg";
-}
+    function abrirPrototipoSnack(){
+      if (protSnack.childElementCount <= 1){
+      document.getElementById("tela2").style = "display: none";
+      var prot = document.createElement("iframe");
+      prot.setAttribute("src","https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FxUaAl9KrNc84CIpIb236Iv%2FRedesign-SnackTrem%3Fpage-id%3D0%253A1%26type%3Ddesign%26node-id%3D1-2%26viewport%3D289%252C295%252C0.26%26t%3DZ1dCYviAZ64qQeQn-1%26scaling%3Dscale-down%26starting-point-node-id%3D1%253A2%26mode%3Ddesign")
+      prot.setAttribute("allowfullscreen", "true");
+      document.getElementById("protSnack").appendChild(prot);
+      }
+    }
